9e24e7ebaf249d44e5efe02c429af8cbeb0a7b15
Rottie <kingtang2180@gmail.com>
2022-02-27 11:54:27 +0800

    heroku

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 // settting main page route
 app.get("/", (req, res) => {
-  res.send("helsslso world");
+  res.send("helso world");
 });
 
 app.use("/employees", employee);
