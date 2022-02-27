d355059845e7d9ee3ce80ef560287645b8e6b1c0
Rottie <kingtang2180@gmail.com>
2022-02-27 17:24:58 +0800

    ds

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const cors = require("cors");
+
 const app = express();
 
 const employee = require("./routes/modules/employee.js");
@@ -8,7 +8,7 @@ const employee = require("./routes/modules/employee.js");
 require("./config/mongoose");
 
 const PORT = process.env.PORT || 5000;
-app.use(cors());
+
 app.use(express.json());
 
 // settting main page route
