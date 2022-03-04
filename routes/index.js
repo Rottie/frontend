9aa5fee6eafe93216e6b8f2429d5003c2513015f
Rottie <kingtang2180@gmail.com>
2022-03-05 00:28:03 +0800

    auth function

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const router = express.Router();
+
 const employee = require("./modules/employee");
+const user = require("./modules/user");
+
 router.use("/", employee);
+router.use("/", user);
 module.exports = router;
